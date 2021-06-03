403240cce043df7448a1024095bd543978f05e5f
Woosiq92 <34748897+Woosiq92@users.noreply.github.com>
2021-06-03 15:29:44 +0900

    Update rand1.c

--- rand1.c
+++ rand1.c
@@ -1,12 +1,13 @@
 #include<stdio.h> 
 #include<stdlib.h> 
 #include<time.h> 
-int main (void) 
+int main(void)
 {
-  int a; 
-  srand((unsigned)time(NULL)); 
-  for ( a= 0; < a<10; a++) 
-    printf("%d", 1+rand()%6); 
-  return 0; 
-  }
-  
+	int a;
+	srand((unsigned)time(NULL));
+
+	for (a = 0; a < 6; a++)
+
+		printf("%d  ", 1 + rand() % 45);
+	return 0;
+}
